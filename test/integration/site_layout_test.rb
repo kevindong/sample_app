ba50d949efa843a98e277f5227ad23f7b45af853
Kevin Dong <kevindong@users.noreply.github.com>
2016-07-05 22:02:21 -0400

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  test "layout links as logged out user" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -9,6 +9,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path, false
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_template 'users/new'
+    assert_select "title", full_title("Sign up")
+  end
+  
+  test "layout links as logged in user" do
+    log_in_as(users(:michael))
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
