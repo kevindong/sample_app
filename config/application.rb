8760c2f73a5f17ec601307519f7fbd2387707e96
Kevin Dong <kevindong@users.noreply.github.com>
2016-07-07 18:37:55 -0400

    Add password reset

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,5 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
   end
 end
